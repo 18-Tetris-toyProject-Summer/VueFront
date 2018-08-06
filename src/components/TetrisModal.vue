6ea4965d81aff4690d61560cbad916453bd195c0
sangchul <1ilsang@naver.com>
2018-08-06 17:43:47 +0900

    [UPDATE] 상하좌우 충돌체크 + 한줄제거 + 점수 + 게임오버

--- src/components/TetrisModal.vue
+++ src/components/TetrisModal.vue
@@ -48,6 +48,7 @@ export default {
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
       ],
       tetrisData: [
@@ -68,31 +69,72 @@ export default {
       curType: 0,
       curColor: 0,
       maxY: 0,
-      minY: 10
+      minY: 10,
+      breakFlag: false,
+      myScore: 0
     }
   },
   watch: {
-    tetrisBoard () {
-      //  console.log('paint!')
-    },
-    curPosition: {
-      handler: function (after, before) {
-        //  TODO X, Y 값 충돌 체크후 멈추기, 한줄 삭제 +
-        // if (after.x === 5) console.log('wow!')
-      },
-      deep: true
-    }
+    // curPosition: {
+    //   handler: function (after, before) {
+    //     //  TODO X, Y 값 충돌 체크후 멈추기, 한줄 삭제 +
+    //     // if (after.x === 5) console.log('wow!')
+    //   },
+    //   deep: true
+    // }
   },
   methods: {
     close () {
       this.$EventBus.$emit('close')
     },
+    checkLine () {
+      let checkPoint = 0
+      let multiPoint = 0
+      for (let i = 1; i < 20; i++) {
+        for (let j = 0; j < 10; j++) {
+          if (this.tetrisBoard[i][j] !== 0 && this.tetrisBoard[i][j] !== '.') {
+            checkPoint++
+          }
+          if (checkPoint === 10) {
+            this.tetrisBoard.splice(i, 1)
+            this.tetrisBoard.unshift([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+            multiPoint++
+          }
+        }
+        checkPoint = 0
+      }
+      //  TODO multiPoint score++
+      this.myScore += multiPoint
+      console.log(this.myScore)
+    },
+    initTetrino () {
+      let nextColor = Math.floor(Math.random() * (8 - 1)) + 1
+      let nextType = Math.floor(Math.random() * (8 - 1))
+      this.curType = nextType
+      this.curColor = nextColor
+      this.curPosition.x = 0
+      this.curPosition.y = 4
+    },
     painting (value) {
       for (let i = 0; i < 4; i++) {
+        if (this.breakFlag) {
+          break
+        }
         for (let j = 0; j < 4; j++) {
           //  FIXME 고정되어 있는 배경에 부딛혔을 때 처리해줘야 함.
           if (this.tetrisData[this.curType][this.curRotation] & (0x8000 >> i * 4 + j)) {
-            if (value) {
+            if (value === 'isCrash') {
+              if ((this.tetrisBoard[this.curPosition.x + i][this.curPosition.y + j] !== 0 &&
+                this.tetrisBoard[this.curPosition.x + i][this.curPosition.y + j] !== '.') ||
+                this.curPosition.x + i + 1 === 21) {
+                this.breakFlag = true
+                if (this.curPosition.x === 1) {
+                  console.log('gameOver')
+                  this.stopGame()
+                }
+                break
+              }
+            } else if (value) {
               this.tetrisBoard[this.curPosition.x + i][this.curPosition.y + j] = value
             }
             if (this.maxY < this.curPosition.y + j) this.maxY = this.curPosition.y + j
@@ -107,31 +149,49 @@ export default {
       this.painting('.')
       if (value === 38) { //  Up
         this.curRotation = (this.curRotation + 1) % 4
-        this.painting()
+        this.painting('isCrash')
         //  FIXME 제일 우축일때 회전이 부자연 스러움.
-        if (this.maxY > 9 || this.minY < 0) {
+        if (this.maxY > 9 || this.minY < 0 || this.breakFlag === true) {
           this.curRotation = (this.curRotation - 1) % 4
+          this.breakFlag = false
+          this.painting(this.curColor)
         }
       } else if (value === 37 && this.minY > 0) { //  Left
         this.curPosition.y--
+        this.painting('isCrash')
+        if (this.breakFlag === true) {
+          this.breakFlag = false
+          this.curPosition.y++
+        }
       } else if (value === 39 && this.maxY < 9) { //  Right
         this.curPosition.y++
+        this.painting('isCrash')
+        if (this.breakFlag === true) {
+          this.breakFlag = false
+          this.curPosition.y--
+        }
       } else if (value === 40) { //  Down
         this.curPosition.x++
+        this.painting('isCrash')
+        if (this.breakFlag === true) {
+          //  TODO 게임오버 멈추는 로직
+          this.curPosition.x--
+          this.breakFlag = false
+          this.painting(this.curColor)
+          this.checkLine()
+          this.initTetrino()
+        }
       }
       this.painting(this.curColor)
-      this.tetrisBoard[19].push(1)
+      this.tetrisBoard[20].push(1)
     },
     keydownEvent (event) {
       this.paintTetris(event.keyCode)
     },
     startGame () {
-      let nextColor = Math.floor(Math.random() * (8 - 1)) + 1
-      let nextType = Math.floor(Math.random() * (8 - 1))
-      this.curType = nextType
-      this.curColor = nextColor
+      this.initTetrino()
       //  TODO Next에 그려줘야 함.
-      this.painting(nextColor)
+      this.painting(this.curColor)
       this.gameflag = setInterval(() => {
         //  한 줄씩 떨어지게 만든다.
         this.paintTetris(40)
@@ -175,7 +235,7 @@ export default {
 #tetris-me{
   position: fixed;
   margin-left: 20%;
-  width: 20%;
+  width: 18%;
   background-color: black;
   border-collapse: collapse;
 }
