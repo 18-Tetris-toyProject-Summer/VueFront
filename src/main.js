72d21f02a0815ed93cefc5ad75e7385c5f15b2e3
sangchul <1ilsang@naver.com>
2018-08-10 04:12:26 +0900

    [Update] JWT Token 쿼리스트링 가져오는것 까지

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import axios from 'axios'
 import router from './router'
 
 Vue.config.productionTip = false
 Vue.prototype.$EventBus = new Vue()
+Vue.prototype.$http = axios
 
 /* eslint-disable no-new */
 new Vue({
